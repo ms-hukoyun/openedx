5f50fb52b7c8ffc2e28cd8714b089b94a79fb20b
Huseyin Koyun <hukoyun@microsoft.com>
2016-09-22 12:26:42 -0700

    For shebang there should not be space between # and !

--- course-migrate/migrate-course.py
+++ course-migrate/migrate-course.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # Copyright (c) Microsoft Corporation. All Rights Reserved.
 # Licensed under the MIT license. See LICENSE file on the project webpage for details.
 import sys, getopt
