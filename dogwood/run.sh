7cacddf56148e65a7522abdb2178e84015aef5bf
ms-hukoyun <hukoyun@microsoft.com>
2016-12-15 20:40:18 -0800

    Fixed a typo. It must be '-t download'

--- dogwood/run.sh
+++ dogwood/run.sh
@@ -1,4 +1,4 @@
 # Create LXC VM for 12.04.5
-# sudo lxc-create -download -n myNode #dist: ubuntu , release: precise , arch: amd64
+# sudo lxc-create -t download -n myNode #dist: ubuntu , release: precise , arch: amd64
 sudo bash install-openedx.sh "named-release/dogwood.rc" | tee /tmp/openedx-install.log
 
